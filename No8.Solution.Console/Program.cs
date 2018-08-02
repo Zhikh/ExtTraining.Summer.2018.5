1957567573fb7669d786e4dec635f148f50fd4c9
Domino <zhikhanastasya@gmail.com>
2018-08-02 23:04:05 +0300

    Add (No8.Solution.Console) basic ui functionality

--- No8.Solution.Console/Program.cs
+++ No8.Solution.Console/Program.cs
@@ -2,52 +2,95 @@
 using No8.Solution.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace No8.Solution.Console
 {
     class Program
     {
+        private enum Action { Add, Remove };
+
         static void Main(string[] args)
         {
             PrinterService printerService = PrinterService.Instance;
             
             while (true)
             {
-                System.Console.WriteLine("Select:");
-                System.Console.WriteLine("0: Add new printer");
-
                 IEnumerable<IPrinter> printers = printerService.GetAll();
 
-                int i = 1;
-                foreach (var printer in printers)
+                PrintMenu(printers);
+                ExucuteAction(printerService, printers);
+            }
+        }
+
+        private static void ExucuteAction(PrinterService printerService, IEnumerable<IPrinter> printers)
+        {
+            string value = System.Console.ReadLine();
+
+            if (int.TryParse(value, out int key))
+            {
+                System.Console.Clear();
+
+                string name, model;
+
+                switch (key)
                 {
-                    System.Console.WriteLine($"{i}: Print on {printer.Name} ({printer.Model})");
-                }
+                    case (int)Action.Add:
+                        GetPrinterInfo(out name, out model);
 
-                var key = System.Console.ReadKey();
+                        printerService.Add(new Printer(name, model));
+                        break;
+                    case (int)Action.Remove:
+                        GetPrinterInfo(out name, out model);
 
-                if (key.Key == ConsoleKey.D1)
-                {
-                    System.Console.Clear();
+                        printerService.Remove(new Printer(name, model));
+                        break;
+                    default:
+                        int i = 1;
 
-                    System.Console.WriteLine("Название:");
-                    var name = System.Console.ReadLine();
+                        if (printers.Count() != 0)
+                        {
+                            i++;
+                        }
 
-                    System.Console.WriteLine("Модель:");
-                    var model = System.Console.ReadLine();
-
-                    printerService.Add(new Printer(name, model));
-
-                    i++;
-                }
-                else
-                {
-                    foreach(var printer in printers)
-                    {
-
-                    }
+                        foreach (var printer in printers)
+                        {
+                            if (key == i)
+                            {
+                                System.Console.WriteLine(printer.Print<string, string>("From"));
+                            }
+                        }
+                        break;
                 }
             }
+        }
+
+        private static void GetPrinterInfo(out string name, out string model)
+        {
+            System.Console.WriteLine("Название:");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Модель:");
+            model = System.Console.ReadLine();
+        }
+
+        private static int PrintMenu(IEnumerable<IPrinter> printers)
+        {
+            int i = 0;
+            System.Console.WriteLine("Select:");
+            System.Console.WriteLine($"{i++}: Add new printer");
+
+            if (printers.Count() != 0)
+            {
+                System.Console.WriteLine($"{i++}: Remove printer");
+            }
+
+            foreach (var printer in printers)
+            {
+                System.Console.WriteLine($"{i++}: Print on {printer.Name} ({printer.Model})");
+            }
+
+            return i;
         }
     }
 }
