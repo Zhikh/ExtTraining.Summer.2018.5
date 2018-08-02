3fc12e4d7e6dfbb1a585332861dd490b06d1cba1
Domino <zhikhanastasya@gmail.com>
2018-08-02 21:53:52 +0300

    Add files

---
+++ No8.Solution.Tests/PrinterServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+using No8.Solution.Interfaces;
+using No8.Solution.Concrete;
+
+namespace No8.Solution.Tests
+{
+    [TestFixture]
+    public class PrinterServiceTests
+    {
+        private IPrinterService printerService;
+
+        [SetUp]
+        public void Init()
+        {
+            printerService = PrinterService.Instance;
+        }
+
+        #region Exceptions
+        [Test]
+        public void Add_NullPrinter_ArgumentNullException() => 
+            Assert.Catch<ArgumentNullException>(() => printerService.Add(null));
+
+        [Test]
+        public void Remove_NullPrinter_ArgumentNullException() =>
+            Assert.Catch<ArgumentNullException>(() => printerService.Remove(null));
+        #endregion
+
+        #region Add printer
+        [TestCase("name1", "model1", "model2", "model3")]
+        public void Add_UniquePrinters_CorrectResult(string name, params string[] models)
+        {
+            foreach (var model in models)
+            {
+                printerService.Add(new Printer(name, model));
+            }
+
+            var printers = printerService.GetAll();
+            int i = 0;
+            foreach(var printer in printers)
+            {
+                Assert.AreEqual(name, printer.Name);
+                Assert.AreEqual(models[i++], printer.Model);
+            }
+        }
+
+        [TestCase(1, "name1", "model1", "model1")]
+        public void Add_SamePrinters_CorrectResult(int dublicate, string name, params string[] models)
+        {
+            foreach (var model in models)
+            {
+                printerService.Add(new Printer(name, model));
+            }
+
+            printerService.Add(new Printer(name, models[dublicate]));
+
+            var printers = printerService.GetAll();
+            int i = 0;
+            foreach (var printer in printers)
+            {
+                Assert.AreEqual(name, printer.Name);
+                Assert.AreEqual(models[i++], printer.Model);
+            }
+        }
+        #endregion
+
+        #region Remove printer
+
+        #endregion
+
+        #region Events
+
+        #endregion
+    }
+}
---
+++ No8.Solution.Tests/PrinterTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using No8.Solution.Concrete;
+using NUnit.Framework;
+using System.IO;
+using System.Diagnostics;
+
+namespace No8.Solution.Tests
+{
+    [TestFixture]
+    public class PrinterTests
+    {
+        #region Exception
+        [Test]
+        public void Print_NullName_ArgumentNullException()
+           => Assert.Catch<ArgumentNullException>(() =>
+           {
+               var printer = new Printer(null, "model");
+           });
+
+        [Test]
+        public void Print_NullModel_ArgumentNullException()
+           => Assert.Catch<ArgumentNullException>(() =>
+           {
+               var printer = new Printer("name", null);
+           });
+
+        [Test]
+        public void Print_EmptyName_ArgumentException()
+          => Assert.Catch<ArgumentException>(() =>
+          {
+              var printer = new Printer("", "model");
+          });
+
+        [Test]
+        public void Print_EmptyModel_ArgumentException()
+          => Assert.Catch<ArgumentException>(() =>
+          {
+              var printer = new Printer("name", "");
+          });
+        #endregion
+
+        [TestCase("name", "model")]
+        public void Printer_CorrectData_SavingInProperties(string name, string model)
+        {
+            var printer = new Printer(name, model);
+
+            Assert.AreEqual(name, printer.Name);
+            Assert.AreEqual(model, printer.Model);
+        }
+
+        [TestCase("test data")]
+        public void Print_FakeData_SameValue(string fakeData)
+        {
+            var printer = new Printer("test", "test");
+
+            var printData = printer.Print<string, string>(fakeData);
+
+            Assert.AreEqual(fakeData, printData);
+        }
+
+        [TestCase(@"C:\Users\Zhikh Anastasya\Documents\ExtTraining.Summer.2018.5\No8.Solution.Tests\bin\Debug\Test.txt")]
+        public void Print_DataFromFile_SameValue(string path)
+        {
+            string expected = FileDataProvider.Load(new FileInfo(path));
+            Debug.Write(expected);
+
+            var printer = new Printer("test", "test");
+            var actual = printer.Print<string, string>(expected);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
